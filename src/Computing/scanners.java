fbbb39c762662778028adb71571eee4fa2095a61
Wotty <william.otty@gmail.com>
2015-10-16 14:35:17 +0100

    Int Summary

--- src/Computing/scanners.java
+++ src/Computing/scanners.java
@@ -7,12 +7,14 @@ public class scanners {
 		
 	}
 	static void checkEligibility(){
-		int age;
+		String age1;
 		int life;
 		Scanner sc= new Scanner(System.in);
 		System.out.println("What is your age?");
-		age=sc.nextInt(); // input captured and stored in age
+		age1=sc.nextLine(); // input captured and stored in age
 		life = 82 - age;
+		if (age1==(int)age1){
+			age = Integer.parseInt(age)}
 		if (age>=18)
 			if (age> 82)
 				if (age >= 123)
@@ -20,12 +22,14 @@ public class scanners {
 				else
 				System.out.println("You're too old you should be dead");
 			else
-			System.out.println("YOU CAN BUY ALCOHOL!!\nYou will die in approximately " + life);
+			System.out.println("You Are old enough to get married\nYou will die in approximately " + life);
 		else if(age < 18)
 				if(age < 0)  
 					System.out.println("Why You lying for?");
 				else
-					System.out.println("You're to young to buy alcohol\nYou will die in approximately " + life + "Years");
+					System.out.println("You're to young to get married\nYou will die in approximately " + life + "Years");
+		else
+			System.out.println("The input Is not an interger");
 	
 	}
 	
