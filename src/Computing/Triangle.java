cc7039e3fff51f317160667ee2247339a5b0f5b9
Wotty <william.otty@gmail.com>
2015-11-03 10:30:55 +0000

    22222

--- src/Computing/Triangle.java
+++ src/Computing/Triangle.java
@@ -1,10 +1,19 @@
 package Computing;
 
+import java.util.Scanner;
+
 public class Triangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input;
+		int count = 0;
+		int numlines;
+		Scanner s= new Scanner(System.in);
+		numlines=s.nextInt(); 
+		for(int i=1; i<numlines; i++){
+			for()
+			
+		}
 	}
 
 }
