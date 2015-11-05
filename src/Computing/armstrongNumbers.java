1d1d8e3cf4efe11081b64bbd79740acded2a5c26
Wotty <william.otty@gmail.com>
2015-11-05 12:16:33 +0000

    122
    123123123123123123123123123

---
+++ src/Computing/armstrongNumbers.java
@@ -0,0 +1,10 @@
+package Computing;
+
+public class armstrongNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Computing/charCounter.java
+++ src/Computing/charCounter.java
@@ -5,14 +5,27 @@ import java.util.Scanner;
 public class charCounter {
 	public static void main(String[]args ){
 	int count = 0;
-	int n;
-	char letter;
+	String str = "helloslkhellodjladfjhello";
+    String findStr = "hello";
 	Scanner scan = new Scanner(System.in);
-    System.out.println("Enter the word you would like to count the number of f's in");
-    n = scan.nextInt();
-    letter = scan.next().charAt(0);
+    System.out.println("Enter the word you would like to count");
+    str = scan.nextLine();
     System.out.println("What character would you like to count");
-			for(int i = )
+    findStr = scan.nextLine();
+    scan.close();
+    
+    int lastIndex = 0;
+
+    while(lastIndex != -1){
+
+        lastIndex = str.indexOf(findStr,lastIndex);
+
+        if(lastIndex != -1){
+            count ++;
+            lastIndex += findStr.length();
+        }
+    }
+    System.out.println("The word "+ str + " contains " +count + " " + findStr +"'s");
 	}
 
 }
