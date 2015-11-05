78fc1d941ce53f4515cf4025f3d319777426fa8e
Wotty <william.otty@gmail.com>
2015-11-05 11:55:04 +0000

    d

---
+++ src/Computing/charCounter.java
@@ -0,0 +1,18 @@
+package Computing;
+
+import java.util.Scanner;
+
+public class charCounter {
+	public static void main(String[]args ){
+	int count = 0;
+	int n;
+	char letter;
+	Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the word you would like to count the number of f's in");
+    n = scan.nextInt();
+    letter = scan.next().charAt(0);
+    System.out.println("What character would you like to count");
+			for(int i = )
+	}
+
+}
