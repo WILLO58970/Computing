c5f654d9540936df850f31cb7fc136e51a44d219
Wotty <william.otty@gmail.com>
2015-11-05 12:17:23 +0000

    2321312321321312312312341234
    1212323

--- src/Computing/armstrongNumbers.java
+++ src/Computing/armstrongNumbers.java
@@ -1,10 +1,14 @@
 package Computing;
 
+import java.util.Scanner;
+
 public class armstrongNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Type in the number that you want to check");
+		scan.nextLine();
+		for(int i = 0; 2 <=)
 	}
 
 }
