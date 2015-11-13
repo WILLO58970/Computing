d3492dcbf55cb957ab25e8d43d585fd2132beb2d
Wotty <william.otty@gmail.com>
2015-11-13 14:34:24 +0000

    4r3434232423423445

--- src/Computing/armstrongNumbers.java
+++ src/Computing/armstrongNumbers.java
@@ -1,12 +1,13 @@
 package Computing;
 import java.math.*;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class armstrongNumbers {
 
 	public static void main(String[] args) {
 		int result = 0;
-		
+		String b = "";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many numbers do you want to add?");
 		int num = sc.nextInt();
@@ -15,22 +16,22 @@ public class armstrongNumbers {
 		int power = sc.nextInt();
 		
 		int[] vars = new int[num];
-		
 		for(int i = 0; i < num; i++) {
 		System.out.println("Enter next Digit:");
 		vars[i] = sc.nextInt();
-		  
+		
 		result = (int) (result + Math.pow(vars[i], power));
 		String word = Integer.toString(result);
-		String b = String.valueOf(vars[i]);
-		String c = b + String.valueOf(vars[i]);
-		if(i==word.length() && word==b){
-			System.out.println("is an armstrong" + vars[i]);
+		b += String.valueOf(vars[i]);
+		if(word.equals(b)){
+			System.out.println("m9 u fuckin done it");
 		}
 		else{
-			System.out.println("nah bruv" + b);
+			System.out.println("nah bruv");
 		}
 		}
+		
+		
 		sc.close();
 		
 		}
