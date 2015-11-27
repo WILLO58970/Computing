0f35ab01b6bda9e9543f5e6d731e67c4881ee863
Wotty <william.otty@gmail.com>
2015-11-27 14:30:48 +0000

    iluio

---
+++ src/Computing/Link.java
@@ -0,0 +1,65 @@
+package Computing;
+
+public class Link {
+	private String name;
+	private int age;
+	private String favBand = "Spice Girls";
+	private boolean inARelationship = true;
+	
+	
+	private Link nextLink = null;
+	
+	public Link(String name,int age,String favBand,boolean inARelationship){
+		this.name=Name;
+		this.age=age;
+		this.favBand=favBand;
+		this.inARelationship = inARelationship;
+		
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public int getAge(){
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getFavBand() {
+		return favBand;
+	}
+	public void setFavBand(String favBand) {
+		this.favBand = favBand;
+	}
+	public boolean isInARelationship() {
+		return inARelationship;
+	}
+	public void setInARelationship(boolean inARelationship) {
+		this.inARelationship = inARelationship;
+	}
+	public Link getNextLink() {
+		return nextLink;
+	}
+	public void setNextLink(Link nextLink) {
+		this.nextLink = nextLink;
+	}
+	public Link getNextLink(){
+		return nextLink;
+	}
+	public void setNextLink(Link nextLink){
+		this.nextLink = nextLink;
+	}
+	public void displayLink(){
+		System.out.println(" ");
+		System.out.println("Name: " + this.getName());
+		System.out.println("Age: " + this.getAge());
+		System.out.println("Band: " + this.getFavBand());
+		System.out.println("Relationship? " + this.isInARelationship());
+		System.out.println(" ");
+		System.out.println("***********");
+	}
+	
+}
---
+++ src/Computing/Linked_List.java
@@ -0,0 +1,16 @@
+package Computing;
+
+public class Linked_List {
+	private Link first;
+	
+	public void Linked_List(){
+		first = null;
+	}
+	
+	public boolean isEmpty(){
+		return (first==null);
+	}
+	public void insertFirst(String name,)
+	
+	
+}
